be44cb3453109f1a80dfa5630d26fd826a1405ca
Satoshi Ikeda <ikeda@koozyt.com>
2017-12-26 22:09:51 +0900

    Apply coding style

--- index.js
+++ index.js
@@ -1,84 +1,96 @@
 // namedpipe client
-var fs = require('fs');
-var log = console.log ;
+const fs = require('fs');
+let log = console.log; // eslint-disable-line no-unused-vars
 
 module.exports = {
-    init: init
-}
+    init: init,
+};
 
-function init(pluginInterface){
+/**
+ * Initialize plugin
+ * @param {object} pluginInterface The interface of picogw plugin
+ */
+function init(pluginInterface) {
     const pi = pluginInterface;
-	log = pi.log;
+    log = pi.log;
 
-	var pipe_prefix = pi.cmd_opts.get('pipe') ;
-	if( !pipe_prefix ) return ;
+    let pipePrefix = pi.cmd_opts.get('pipe');
+    if (!pipePrefix) return;
 
-	// Pipe postfix
-	// _r (read port from client's viewpoint)
-	// _w (write port from client's viewpoint)
-	const PIPE_NAME = {read:pipe_prefix+'_w',write:pipe_prefix+'_r'} ;
+    // Pipe postfix
+    // _r (read port from client's viewpoint)
+    // _w (write port from client's viewpoint)
+    const PIPE_NAME = {read: pipePrefix+'_w', write: pipePrefix+'_r'};
 
-	function onerror(msg){
-	    console.error('Error in communicating with named pipe '+pipe_prefix+'_r/_w:');
-	    console.error(msg);
-	    console.error('Stoped using named pipe.');
-	}
-	try {
-		console.log('Connecting to named pipe '+pipe_prefix+'_r/_w (block until target process is connected.)') ;
-		// Read stream setup
-		var rs = fs.createReadStream(PIPE_NAME.read, 'utf-8');
-		var ws ;
+    const onerror = (msg) => {
+        console.error('Error in communicating with named pipe '+pipePrefix+'_r/_w:'); // eslint-disable-line max-len
+        console.error(msg);
+        console.error('Stoped using named pipe.');
+    };
+    try {
+        console.log('Connecting to named pipe '+pipePrefix+'_r/_w (block until target process is connected.)'); // eslint-disable-line max-len
+        // Read stream setup
+        let rs = fs.createReadStream(PIPE_NAME.read, 'utf-8');
+        let ws;
 
-		var readbuf = '' ;
-		rs	.on('data', data=>{
-				readbuf += data ;
-				var ri = readbuf.lastIndexOf("\n") ;
-				if( ri<0 ) return ;
-				var focus = readbuf.slice(0,ri) ;
-				readbuf = readbuf.slice(ri+1) ;
+        let readbuf = '';
+        rs.on('data', (data)=>{
+            readbuf += data;
+            let ri = readbuf.lastIndexOf('\n');
+            if (ri<0) return;
+            let focus = readbuf.slice(0, ri);
+            readbuf = readbuf.slice(ri+1);
 
-				focus.split("\n").forEach(req_str=>{
-					var req = JSON.parse(req_str) ;
-					if( req.method.toUpperCase() == 'SUB' ){
-						pi.client.subscribe(req.path,re=>{
-							ws.write(JSON.stringify(re)) ;
-						}) ;
-						ws.write(JSON.stringify({success:true,tid:req.tid}));
-					} else if( req.method.toUpperCase() == 'UNSUB' ){
-						pi.client.unsubscribeall(req.path) ;
-						ws.write(JSON.stringify({success:true,tid:req.tid}));
-					} else {
-						pi.client.callproc(req).then(re=>{
-							re.tid = req.tid ;
-							ws.write(JSON.stringify(re)+"\n") ;
-						}).catch(e=>{
-							e.tid = req.tid ;
-							ws.write(JSON.stringify(e)+"\n") ;
-						}) ;
-					}
-				}) ;
-		    })
-	    	.on('open',()=>{		console.log('Read pipe opened.');})
-	    	.on('error', err =>{	onerror(JSON.stringify(err)); })
-	    	.on('close',()=>{
-	    		pi.client.unsubscribeall();
-	    		onerror('Read pipe closed.');
-	    	}) ;
+            focus.split('\n').forEach((reqStr)=>{
+                let req = JSON.parse(reqStr);
+                if (req.method.toUpperCase() == 'SUB') {
+                    pi.client.subscribe(req.path, (re)=>{
+                        ws.write(JSON.stringify(re));
+                    });
+                    ws.write(JSON.stringify({success: true, tid: req.tid}));
+                } else if (req.method.toUpperCase() == 'UNSUB') {
+                    pi.client.unsubscribeall(req.path);
+                    ws.write(JSON.stringify({success: true, tid: req.tid}));
+                } else {
+                    pi.client.callproc(req).then((re)=>{
+                        re.tid = req.tid;
+                        ws.write(JSON.stringify(re)+'\n');
+                    }).catch((e)=>{
+                        e.tid = req.tid;
+                        ws.write(JSON.stringify(e)+'\n');
+                    });
+                }
+            });
+        })
+            .on('open', ()=>{
+                console.log('Read pipe opened.');
+            })
+            .on('error', (err) =>{
+                onerror(JSON.stringify(err));
+            })
+            .on('close', ()=>{
+                pi.client.unsubscribeall();
+                onerror('Read pipe closed.');
+            });
 
-	    // Write stream setup
-	    ws = fs.createWriteStream(PIPE_NAME.write, 'utf-8');
-	    ws	.on('drain', ()=>{})
-	    	.on('open',()=>{		console.log('Write pipe opened.');})
-	        .on('error', err =>{ onerror(JSON.stringify(err)); })
-	        .on('close', ()=>{
-	    		pi.client.unsubscribeall();
-	        	onerror('Write pipe closed.');
-	        })
-	        //.on('pipe',  src =>{});
-	} catch (err) {
-		//console.error(err) ;
-	    console.error('Error in named pipe communication.');
-	    console.error('Stoped using named pipe.');
-   		pi.client.unsubscribeall();
-	}
+        // Write stream setup
+        ws = fs.createWriteStream(PIPE_NAME.write, 'utf-8');
+        ws .on('drain', ()=>{})
+            .on('open', ()=>{
+                console.log('Write pipe opened.');
+            })
+            .on('error', (err) =>{
+                onerror(JSON.stringify(err));
+            })
+            .on('close', ()=>{
+                pi.client.unsubscribeall();
+                onerror('Write pipe closed.');
+            });
+        // .on('pipe',  src =>{});
+    } catch (err) {
+        // console.error(err) ;
+        console.error('Error in named pipe communication.');
+        console.error('Stoped using named pipe.');
+        pi.client.unsubscribeall();
+    }
 }
